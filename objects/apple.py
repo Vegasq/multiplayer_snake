5187a24070a070cf896c468be8a3af978f51274b
Mykola Yakovliev <vegasq@gmail.com>
2017-06-23 15:32:07 -0500

    New Collider

--- objects/apple.py
+++ objects/apple.py
@@ -4,7 +4,7 @@ from objects.common import Cell, CellStack, Killable
 class Apple(CellStack, Killable):
     def __init__(self, x, y):
         super(Apple, self).__init__()
-        c = AppleCell(x, y)
+        c = AppleCell(x, y, hardness=1)
         c.set_owner(self)
         self.add_cell(c)
 
